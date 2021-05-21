2c68fedc8e5246e29115e2f3164e0d82612a18d9
Avisa-SEI <aap571420@gmail.com>
2021-05-21 16:38:43 -0700

    add bootstrap

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const apiAddress = `http://api.openweathermap.org/data/2.5/forecast?q=houston&cn
 /*----- event listeners -----*/
 /*----- functions -----*/
 
+$('h1').addClass('text-center');
+
 const response = $.ajax({url: 'http://api.openweathermap.org/data/2.5/forecast?q=houston&cnt=5&appid=c8f827699e922be9a613a16ce1a00f37&units=imperial'}).then( (data) => {
     // success
     console.log(data['city']['name'])
