3ce099ee94a3d1fa37adb2b8389cf14771f92eae
Avisa-SEI <aap571420@gmail.com>
2021-05-21 17:14:28 -0700

    feat: Add data to table

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let weatherData;
+let temperature, feelsLike, descript, cityName
 /*----- cached element references -----*/
 const apiAddress = `http://api.openweathermap.org/data/2.5/forecast?q=houston&cnt=5&appid=c8f827699e922be9a613a16ce1a00f37&units=imperial`
+const $table = $(`#homes tbody`)
 /*----- event listeners -----*/
 /*----- functions -----*/
 
@@ -11,15 +13,30 @@ $('h1').addClass('text-center');
 
 const response = $.ajax({url: 'http://api.openweathermap.org/data/2.5/forecast?q=houston&cnt=5&appid=c8f827699e922be9a613a16ce1a00f37&units=imperial'}).then( (data) => {
     // success
-    console.log(data['city']['name'])
+   cityName = data['city']['name']
 
-    data['list'].forEach(element => {
-        console.log(element['main']['temp'])
-        console.log(element['main']['feels_like'])
-        element['weather'].forEach(function(elem) {
-            console.log(elem['description'])
+    data['list'].forEach(function(element, idx) {
+        $table.append(`
+        <tr>
+        <td>Day ${idx + 1}</td>
+        <td>${element['main']['temp']}</td>
+        <td>${element['main']['feels_like']}</td>
+        
+      <td>${element['weather'].map(function(elem) {
+            return elem['description']
         })
+    }</td>`)
+
     });
+
+    // $table.append(` 
+    //     <tr>
+    //     <td>${cityName}</td>
+    //     <td>${temperature}</td>
+    //     <td>${feelsLike}</td>
+    //     <td>${descript}</td>
+    // </tr>
+    // `);
 
 },
     (error) => {
