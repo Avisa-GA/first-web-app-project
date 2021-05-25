6698982a51112f1d04307b351d4b630a0c0773a4
Avisa-SEI <aap571420@gmail.com>
2021-05-25 14:34:22 -0700

    Add google account for api, and zoom closer

--- js/script.js
+++ js/script.js
@@ -1,53 +1,56 @@
-/*----- constants -----*/
-const $table = $(`#homes tbody`)
-const $cityName = $(`th#city`)
-const $mainContent = $('tbody#dataBody')
-const $mainBody = $('body')
-/*----- app's state (variables) -----*/
-let weatherData, userInput;
+$(function () {
+    /*----- constants -----*/
+    const $table = $(`#homes tbody`)
+    const $cityName = $(`th#city`)
+    const $mainContent = $('tbody#dataBody')
+    const $mainBody = $('body')
+    /*----- app's state (variables) -----*/
+    let weatherData, userInput;
 
-/*----- cached element references -----*/
+    /*----- cached element references -----*/
 
-/*----- event listeners -----*/
-$('form').on('submit', handleGetData)
-/*----- functions -----*/
+    /*----- event listeners -----*/
 
-init()
+    $('form').on('submit', handleGetData)
 
-function init() {
-    $('h1').addClass('text-center');
-}
+    /*----- functions -----*/
 
-function handleGetData(event) {
+    init()
 
-    $mainContent.empty()
+    function init() {
+        $('h1').addClass('text-center');
+    }
 
-    event.preventDefault()
+    function handleGetData(event) {
 
-    userInput = $('input#search').val()
+        $mainContent.empty()
 
-    const response = $.ajax({
-        url: `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&cnt=5&appid=c8f827699e922be9a613a16ce1a00f37&units=imperial`
-    }).then((data) => {
+        event.preventDefault()
 
-            // success
-            weatherData = data
-            render()
-            initMap()
-        },
-        (error) => {
-            console.log('Bad Request: ', error)
-        }
-    )
-    $('input#search').val('')
-}
+        userInput = $('input#search').val()
 
-function render() {
+        const response = $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&cnt=5&appid=c8f827699e922be9a613a16ce1a00f37&units=imperial`
+        }).then((data) => {
 
-    $cityName.text(`${weatherData['city']['name']}`)
+                // success
+                weatherData = data
+                render()
+                initMap()
+            },
+            (error) => {
+                console.log('Bad Request: ', error)
+            }
+        )
+        $('input#search').val('')
+    }
 
-    weatherData['list'].forEach(function (element, idx) {
-        $table.append(`
+    function render() {
+
+        $cityName.text(`${weatherData['city']['name']}`)
+
+        weatherData['list'].forEach(function (element, idx) {
+            $table.append(`
         <tr>
         <td>${idx + 1} )</td>
         <td>${element['main']['temp']}</td>
@@ -57,25 +60,27 @@ function render() {
         })
     }</td></tr>`)
 
-    });
-}
-
-
-function initMap() {
-
-    let place = {
-        lat: weatherData['city']['coord']['lat'],
-        lng: weatherData['city']['coord']['lon']
+        });
     }
 
-    let map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: place
-    })
 
-    $mainBody.append(`
+    function initMap() {
+
+        let place = {
+            lat: weatherData['city']['coord']['lat'],
+            lng: weatherData['city']['coord']['lon']
+        }
+
+        let map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 10,
+            center: place
+        })
+
+        $mainBody.append(`
     <script>
     ${new google.maps.Marker({position: place, map: map})}
     </script>
     `)
-}
+    }
+
+})
